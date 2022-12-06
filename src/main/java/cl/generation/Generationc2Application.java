5a4efc6ff8a42bcf99bc3f78f5fd4fd64e5ddb8f
Israel Alexis Palma Quezada <ipalm001@gmail.com>
2022-12-06 11:35:11 -0300

    pagina de inicio por default index.jsp

--- src/main/java/cl/generation/Generationc2Application.java
+++ src/main/java/cl/generation/Generationc2Application.java
@@ -1,13 +1,30 @@
 package cl.generation;
 
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.ajp.AbstractAjpProtocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Generationc2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Generationc2Application.class, args);
+	}
+
+	@Bean
+	public TomcatServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+		Connector ajpConnector = new Connector("AJP/1.3");
+		ajpConnector.setPort(9090);
+		ajpConnector.setSecure(false);
+		ajpConnector.setAllowTrace(false);
+		ajpConnector.setScheme("http");
+		((AbstractAjpProtocol) ajpConnector.getProtocolHandler()).setSecretRequired(false);
+		tomcat.addAdditionalTomcatConnectors(ajpConnector);
+		return tomcat;
 	}
 
 }
